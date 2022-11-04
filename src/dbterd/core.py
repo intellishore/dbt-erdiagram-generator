3748e335e8dfa9939fa3b2e47084712bc90d4b5e
Manish Khatri <manish.khatri@moo.com>
2022-11-04 11:39:30 +0000

    Support multiple schemas for models and seeds

--- src/dbterd/core.py
+++ src/dbterd/core.py
@@ -1,34 +1,71 @@
 import json
 import yaml
 import re
+import glob
+from typing import List, Tuple
 
-def loadModel(catalog_path, schema_path):
-    """Loads the dbt catalog and schema. The schema selected is the one that will be used to generate the ERD diagram.
+
+def load_catalog(catalog_path: str) -> dict:
+    """Loads the dbt catalog.json file
 
     Args:
-        catalog_path (Path): Path to dbt catalog
-        schema_path (Path): Path to dbt schema
+        catalog_path (str): path to the catalog.json file
 
     Returns:
-        dict, dict: Return schema and catalog dicts.  
-    """    
+        dict: Parses the contents and returns the Dictionary equivalent
+    """
     with open(catalog_path) as f:
         catalog = json.load(f)
 
-
-    with open(schema_path, 'r') as f:
-        schema = yaml.safe_load(f)
-
-    return catalog, schema
+    return catalog
 
 
-def createTable(dbml_path, model):
-    """Create a table in the dbml file. 
+def load_schemas(path: str) -> List[dict]:
+    """Load all *.yaml files under the provided file path.py
+    File search is recursive
+
+    Args:
+        path (str): file path to search under
+
+    Returns:
+        List[dict]: list of schemas
+    """
+    schemas = []
+    files = glob.glob(path + "/**/*.yml", recursive=True)
+
+    for file_path in files:
+        with open(file_path, "r") as f:
+            schema = yaml.safe_load(f)
+            schemas.append(schema)
+
+    return schemas
+
+
+def load_model(catalog_path: str, model_path: str, seed_path: str) -> Tuple[dict, List[dict], List[dict]]:
+    """Loads the dbt catalog and model+seeds schemas.
+
+    Args:
+        catalog_path (str): Path to dbt catalog
+        model_path (str): Path to the root of the dbt model schemas
+        seed_path (str): Path to the root of the dbt seed schemas
+
+    Returns:
+        Tuple[dict, List[dict], List[dict]]: catalog, model schemas and seed schemas
+    """
+    catalog = load_catalog(catalog_path)
+    model_schemas = load_schemas(model_path)
+    seed_schemas = load_schemas(seed_path)
+
+    return catalog, model_schemas, seed_schemas
+
+
+def create_table(dbml_path, model) -> None:
+    """Create a table in the dbml file.
 
     Args:
         dbml_path (dbml file): The file where to store the table
         model (dbt model): The dbt model to extract the table and columns from
-    """    
+    """
     name = model["metadata"]["name"]
     columns = list(model["columns"].keys())
     start = "{"
@@ -37,55 +74,64 @@ def createTable(dbml_path, model):
     dbml_path.write(f"Table {name} {start} \n")
 
     for column_name in columns:
-        column = model["columns"][column_name] 
-        name = column["name"]
+        column = model["columns"][column_name]
+        # some fields come back with casting, we only want the column name. E.g. RETAIL_AMOUNT::NUMBER(38,2)
+        name = column["name"].split("::")[0]
         dtype = column["type"]
 
         dbml_path.write(f"{name} {dtype} \n")
     dbml_path.write(f"{end} \n")
-    
-    
-def createRelatonship(dbml_path, schema):
-    """Create a relationship in the dbml file. Loops over all columns to find relationship tests and saves them to the dbml file
+
+
+def create_relationship(dbml_path: str, models: List[dict]) -> None:
+    """Create a relationship in the dbml file. Loops over all columns to find relationship tests
+    and saves them to the dbml file
 
     Args:
-        dbml_path (dbml file): The file where to store the table
-        schema (dbt schema): The dbt schema to extract relationships from 
-    """    
-    for model in schema["models"]:
+        dbml_path (str): The file where to save the table dbml
+        models (List[dict]): The List of dbt model schemas to extract relationships from
+    """
+    for model in models:
         for column in model["columns"]:
             if "tests" in column:
                 tests = column["tests"]
                 for test in tests:
-                    if isinstance(test, dict): 
+                    if isinstance(test, dict):
                         if "relationships" in test:
                             relationship = test["relationships"]
                             r1 = relationship["to"].upper()
                             r1 = re.findall(r"('.*?')", r1, re.DOTALL)[0].replace("'", "")
                             r1_field = relationship["field"].upper()
-                            
+
                             r2 = model["name"].upper()
                             r2_field = column["name"].upper()
                             dbml_path.write(f"Ref: {r1}.{r1_field} > {r2}.{r2_field} \n")
-                            
 
 
-def genereatedbml(schema_path, catalog_path, dbml_path):
+def generate_dbml(catalog_path: str, model_path: str, seed_path: str, dbml_path: str) -> None:
     """Create dbml file for a dbt schema
 
     Args:
-        catalog_path (Path): Path to dbt catalog
-        schema_path (Path): Path to dbt schema
-        dbml_path (Path): Pat to save dbml file 
-    """    
-    catalog, schema = loadModel(catalog_path, schema_path)
+        catalog_path (str): Path to dbt catalog
+        model_path (str): Path to dbt model schemas
+        seed_path (str): Path to dbt seed schemas
+        dbml_path (str): Path to save dbml file to
+    """
+    catalog, model_schemas, seed_schemas = load_model(catalog_path, model_path, seed_path)
+
     model_names = catalog["nodes"]
-    
-    tables = [model["name"].upper() for model in schema["models"]]
-    
+    models = [schema["models"][0] for schema in model_schemas if schema.get("models")]
+    seeds = [schema["seeds"][0] for schema in seed_schemas if schema.get("seeds")]
+
+    tables = [model["name"].upper() for model in models]
+    seed_tables = [model["name"].upper() for model in seeds]
+    tables.extend(seed_tables)
+
     with open(dbml_path, "w") as dbml_file:
         for model_name in model_names:
             model = catalog["nodes"][model_name]
-            if model["metadata"]["name"] in tables: 
-                createTable(dbml_file, model)        
-        createRelatonship(dbml_file, schema)
+            if model["metadata"]["name"] in tables:
+                create_table(dbml_file, model)
+
+        create_relationship(dbml_file, models)
+        create_relationship(dbml_file, seeds)
--- src/dbterd/terminal.py
+++ src/dbterd/terminal.py
@@ -1,24 +1,22 @@
 import click
-from .core import genereatedbml
+from .core import generate_dbml
 import subprocess
 
 
 @click.command()
-@click.argument('schema_path', type=str)
-@click.argument('catalog_path', type=str)
-@click.argument('erd_path', type=str)
-@click.argument('project_name', type=str)
-@click.argument('visualize', type=bool)
-def cli(schema_path, catalog_path, erd_path, project_name, visualize):
-    """"Generate a DBML file from a dbt project and visualize it with dbdocs.io"""
-    try:
-        genereatedbml(schema_path, catalog_path, erd_path)
-    except:
-        print("The dbml file does not match the required format")
-        
+@click.argument("catalog_path", type=str, default="./target/catalog.json")
+@click.argument("model_path", type=str, default="./models")
+@click.argument("seed_path", type=str, default="./seeds")
+@click.argument("erd_path", type=str, default="dbt_erd.dbml")
+@click.argument("project_name", type=str, default="")
+@click.argument("visualize", type=bool, default=False)
+def cli(catalog_path, model_path, seed_path, erd_path, project_name, visualize):
+    """ "Generate a DBML file from a dbt project and visualize it with dbdocs.io"""
+
+    generate_dbml(catalog_path, model_path, seed_path, erd_path)
+
     if visualize:
         try:
             subprocess.run(f"dbdocs build {erd_path} --project {project_name}", text=True, shell=True)
-        except:
+        except Exception:
             print("dbdocs is not set up probably")
-            
