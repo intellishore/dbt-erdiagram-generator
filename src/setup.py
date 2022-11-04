e070022a19087bc7666c276e43340b41c7d6b01c
Manish Khatri <manish.khatri@moo.com>
2022-11-04 11:39:01 +0000

    Linting

--- src/setup.py
+++ src/setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dbterd',
-    version='0.0.1',    
-    description='A simple package that can generate dbml file and erd diagrams for dbt',
-    url='https://github.com/intellishore/dbt-erdiagram-generator',
-    author='Oliver Rise Thomsen, Anders Boje Hertz',
-    author_email='oliver.thomsen@intellishore.dk',
-    license='MIT',
+    name="dbterd",
+    version="0.0.2",
+    description="A simple package that can generate dbml file and erd diagrams for dbt",
+    url="https://github.com/intellishore/dbt-erdiagram-generator",
+    author="Oliver Rise Thomsen, Anders Boje Hertz",
+    author_email="oliver.thomsen@intellishore.dk",
+    license="MIT",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['pyyaml', 'Click'],
-    
-    entry_points='''
+    install_requires=["pyyaml", "Click"],
+    entry_points="""
         [console_scripts]
         dbterd=dbterd.terminal:cli
-    ''',
+    """,
 )
